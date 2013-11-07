7bbf275c5e000ff72ce73e91195a4adb3824f97e
Lukas Diacik <lukas.diacik@gmail.com>
2013-11-07 16:00:22 +0100

    convert tabs to spaces

--- spec/EntityArraySpec.js
+++ spec/EntityArraySpec.js
@@ -1,51 +1,51 @@
 describe("jQuery Entity Array plugin", function () {
-	var ea;
+    var ea;
 
-	beforeEach(function () {
-		setFixtures("<div id=\"entity-array\"/>");
-		ea = $("#entity-array");
-	});
+    beforeEach(function () {
+        setFixtures("<div id=\"entity-array\"/>");
+        ea = $("#entity-array");
+    });
 
-	it("can be initialized", function () {
-		ea.entityArray();
-		expect(ea).toHaveClass("entity-array");
-	});
+    it("can be initialized", function () {
+        ea.entityArray();
+        expect(ea).toHaveClass("entity-array");
+    });
 
-	it("generates html for entities provided in options", function () {
-		var children,
-			entities = [
-				{
-					id: 1,
-					name: 'Peter'
-				},
-				{
-					id: 2,
-					name: 'Paul'
-				},
-				{
-					id: 3,
-					name: 'Catherine'
-				}
-			];
+    it("generates html for entities provided in options", function () {
+        var children,
+            entities = [
+                {
+                    id: 1,
+                    name: 'Peter'
+                },
+                {
+                    id: 2,
+                    name: 'Paul'
+                },
+                {
+                    id: 3,
+                    name: 'Catherine'
+                }
+            ];
 
-		ea.entityArray({
-			entities: entities
-		});
+        ea.entityArray({
+            entities: entities
+        });
 
-		children = ea.children()
-		expect(children).toHaveLength(entities.length);
+        children = ea.children()
+        expect(children).toHaveLength(entities.length);
 
-		$.each(entities, function (i) {
-			expect($(children[i])).toHaveText(this.name);
-			expect($('input', children[i])).toHaveValue(this.id);
-		});
-	});
+        $.each(entities, function (i) {
+            expect($(children[i])).toHaveText(this.name);
+            expect($('input', children[i])).toHaveValue(this.id);
+        });
+    });
 
-	it("removes entities on click", function () {
+    it("removes entities on click", function () {
 
-	});
+    });
 
-	it("has an add method", function () {
+    it("has an add method", function () {
 
-	});
+    });
 });
